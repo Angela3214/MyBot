c8b033e4ac58ba7f4001fe7c09e41ad7a5beb741
Angelika Dashkevich <angela_volkova_up@mail.ru>
2021-12-22 11:28:39 +0300

    Test version

---
+++ Code.py
@@ -0,0 +1,71 @@
+import telebot
+from telebot import types
+import datetime
+from threading import Thread
+
+bot = telebot.TeleBot('5019599335:AAFvC46wOT3vX2GK-53gqLyJwBm8yQowWZM')
+mp = dict()
+
+@bot.message_handler(commands=['start'])
+def button_message(message):
+    user_name = message.from_user.first_name
+    bot.send_message(message.chat.id, 'Теперь ты никогда не будешь забывать поздравить друзей с Днём Рождения, ' + user_name)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Добавить День Рождения")
+    item2 = types.KeyboardButton("Удалить День Рождения")
+    item3 = types.KeyboardButton("Вывести созданные данные")
+    markup.add(item1)
+    markup.add(item2)
+    markup.add(item3)
+    bot.send_message(message.chat.id, 'Выберите нужную функцию', reply_markup=markup)
+
+
+ok = 0
+
+
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    global ok
+
+    if message.text == "Добавить День Рождения":
+        bot.send_message(message.chat.id, 'Пожалуйста, вводите уникальные имена пользователей')
+        bot.send_message(message.chat.id, 'Введите имя и дату рождения в формате ИМЯ#ДД.ММ.ГГГГ')
+        ok = 1
+        return
+    elif message.text == "Удалить День Рождения":
+        bot.send_message(message.chat.id, 'Пожалуйста, вводите корректное имя, которое добавляли ранее')
+        bot.send_message(message.chat.id, 'Введите имя пользователя, которого желаете удалить')
+        ok = 2
+        return
+    elif message.text == "Вывести созданные данные":
+        for el in mp:
+            bot.send_message(message.chat.id, el + ' : ' + mp[el] + '\n')
+    if ok == 1:
+        ind_end_name = message.text.index('#')
+        name = message.text[:ind_end_name]
+        data = message.text[ind_end_name + 1:]
+        mp[name] = data
+        bot.send_message(message.chat.id, 'Ух ты, успешно добавил!')
+    elif ok == 2:
+        if mp.get(message.text) == None:
+            bot.send_message(message.chat.id, 'Ну я же попросил...Ошибка: такого пользователя нет')
+        else:
+            mp.pop(message.text)
+            bot.send_message(message.chat.id, 'Я удалиль, теперь этого клоуна нет в твоих данных')
+    ok = 0
+
+def start():
+    bot.polling(none_stop=True, interval=1)
+
+#@bot.message_handler(content_types='text')
+"""def Check_birthday():
+    for el in mp:
+        impl_data = datetime.date(mp[el][4:], mp[el][2:4], mp[el][:2])
+        if impl_data == datetime.date.today():
+            bot.send_message(id_user, 'Не забудь поздравить ' + el + ' с Днём Рождеия')
+    bot.polling(none_stop=True, interval=8) """
+#tm1 = Thread(target=start)
+#tm2 = Thread(target=Check_birthday)
+# tm2.start()
+# tm1.start()
+start()
