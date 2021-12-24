82c52346c099bb6235a412ef4275d0e454f1fb87
Angelika Dashkevich <angela_volkova_up@mail.ru>
2021-12-25 01:11:11 +0300

    Test version1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 import telebot
 
-form = '{{:<{10}}}'
+Form = '{{:<{10}}}'
 
 conn1 = sqlite3.connect('telegram_bot.db')
 curs1 = conn1.cursor()
@@ -92,7 +92,7 @@ def message_reply(message):
                 f'select note, d_birthday from birthdays where id_tel = {human_id}'):
             for lines in curs.fetchall():
                 bot.send_message(human_id,
-                                 ''.join(form.format(line) for line in lines))
+                                 ''.join(Form.Format(line) for line in lines))
     answer(label, conn, curs, message, human_id)
     curs.close()
 
