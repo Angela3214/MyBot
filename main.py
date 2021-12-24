4212ee1daa57de6b5ea41c494498ed404b53d8e1
Angelika Dashkevich <angela_volkova_up@mail.ru>
2021-12-25 01:19:06 +0300

    Test version1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import telebot
 FORM = '{{:<{10}}}'
 conn1 = sqlite3.connect('telegram_bot.db')
 curs1 = conn1.cursor()
-curs1.execute('''create table if not exists birthdays(id_tel varchar2(200), 
+curs1.execute('''create table if not exists birthdays(id_tel varchar2(200),
 note varchar2(1000), d_birthday date)''')
 curs1.close()
 
