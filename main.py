fa4e30c36320201f62f6e3837fde46e6e5287334
Angelika Dashkevich <angela_volkova_up@mail.ru>
2021-12-25 03:48:27 +0300

    Test version1

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ curs1.execute('''create table if not exists birthdays(id_tel varchar2(200),
 note varchar2(1000), d_birthday date)''')
 curs1.close()
 
-bot = telebot.TeleBot(os.environ['my_token'])
+#bot = telebot.TeleBot(os.environ['my_token'])
+bot = telebot.TeleBot('5019599335:AAFvC46wOT3vX2GK-53gqLyJwBm8yQowWZM')
+
+mp = dict()
 
 
 @bot.message_handler(commands=['start'])
@@ -31,77 +34,76 @@ def button_message(message):
     bot.send_message(message.chat.id, 'Выберите нужную функцию', reply_markup=markup)
 
 
-def answer(label, conn, curs, message, human_id):
+def answer(label, message, human_id):
     """Implementing every label"""
+    bot.send_message(human_id, 'Секундочку')
+    conn3 = sqlite3.connect('telegram_bot.db')
+    curs3 = conn3.cursor()
     if label == 1:
         ind_end_name = message.text.index('#')
         name = message.text[:ind_end_name]
         data = message.text[ind_end_name + 1:]
-        if curs.execute(
+        if curs3.execute(
                 f'insert into birthdays values(\'{human_id}\', \'{name}\', \'{data}\')'):
-            conn.commit()
+            conn3.commit()
             bot.send_message(human_id, 'Ух ты, успешно добавил!')
 
     elif label == 2:
         text = message.text
-        curs.execute(
+        curs3.execute(
             f'delete from birthdays where id_tel = {human_id} and note = \'{text}\'')
-        curs.execute(
+        curs3.execute(
             f'select * from birthdays where id_tel = {human_id} and note = \'{text}\'')
-        test = curs.fetchall()
+        test = curs3.fetchall()
         print(test)
         if not test:
-            conn.commit()
+            conn3.commit()
             bot.send_message(human_id, 'Я удалиль, теперь этого клоуна нет в твоих данных')
         else:
             bot.send_message(human_id, 'Ну я же попросил...Ошибка: такого пользователя нет')
     elif label == 3:
         tomorrow = (date.today() + timedelta(days=1)).strftime("%d.%m")
-        if curs.execute(
+        if curs3.execute(
                 f'select id_tel, note from birthdays where d_birthday like \'{tomorrow}%\''):
-            for line in curs.fetchall():
+            for line in curs3.fetchall():
                 bot.send_message(line[0], 'Не забудь поздравить ' + line[1] + ' с Днём Рождеия')
         else:
             bot.send_message(human_id, 'Никого неть :c')
+    mp.pop(human_id)
 
 
 @bot.message_handler(content_types='text')
 def message_reply(message):
     """implementing answers"""
-    label = 0
     conn = sqlite3.connect('telegram_bot.db')
     curs = conn.cursor()
     human_id = message.chat.id
+    if human_id in mp.keys():
+        answer(mp[human_id], message, human_id)
     if message.text == "Добавить День Рождения":
         bot.send_message(human_id,
                          'Для удобства использования вводите '
                          'уникальные записки пользователей')
         bot.send_message(human_id, 'Введите записку и дату рождения в формате ИМЯ#ДД.ММ.ГГГГ')
-        label = 1
+        mp[human_id] = 1
     elif message.text == "Удалить День Рождения":
         bot.send_message(human_id, 'Пожалуйста, вводите корректную записку, '
                                    'которую добавляли ранее')
         bot.send_message(human_id, 'Введите записку, которую желаете удалить')
-        label = 2
+        mp[human_id] = 2
     elif message.text == "Проверить Дни Рождения":
-        label = 3
+        mp[human_id] = 3
     elif message.text == "Вывести созданные данные":
         if curs.execute(
-                f'select note, d_birthday from birthdays where id_tel = {human_id}'):
+                'select note, d_birthday from birthdays where id_telegram = {hunam_id}'):
             for lines in curs.fetchall():
-                bot.send_message(human_id,
-                                 ''.join(FORM.format(line) for line in lines))
-    answer(label, conn, curs, message, human_id)
+                bot.send_message(human_id, ''.join(FORM.format(line) for line in lines))
     curs.close()
-
-
-# except Exception as element:
-# print(element)
 
 
 def start():
     """Start out bot"""
-    bot.polling(none_stop=True, interval=0)
+    bot.polling(none_stop=True, interval=1)
 
 
 @bot.message_handler(content_types='text')
