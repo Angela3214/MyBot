39c9a80a492d3081979de75a452d2b23d51ca88f
Angelika Dashkevich <angela_volkova_up@mail.ru>
2021-12-25 16:56:59 +0300

    Test version1

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 """This is the code of my bot"""
 from datetime import timedelta, date
-from threading import Thread
-from time import sleep
 import os
 import sqlite3
 import telebot
@@ -13,10 +11,10 @@ curs1.execute('''create table if not exists birthdays(id_tel varchar2(200),
 note varchar2(1000), d_birthday date)''')
 curs1.close()
 
-# bot = telebot.TeleBot(os.environ['my_token'])
+#bot = telebot.TeleBot(os.environ['my_token'])
 bot = telebot.TeleBot('5019599335:AAFvC46wOT3vX2GK-53gqLyJwBm8yQowWZM')
 
-mp = dict()
+mp = {}
 
 
 @bot.message_handler(commands=['start'])
@@ -53,15 +51,16 @@ def answer(label, message, human_id):
     elif label == 2:
         text = message.text
         curs3.execute(
+            f'delete from birthdays where id_tel = {human_id} and note = \'{text}\'')
+        curs3.execute(
             f'select * from birthdays where id_tel = {human_id} and note = \'{text}\'')
-        if not curs3.fetchall():
-            bot.send_message(human_id, 'Ну я же попросил...Ошибка: такого пользователя нет')
-        else:
-            curs3.execute(
-                f'delete from birthdays where id_tel = {human_id} and note = \'{text}\'')
+        test = curs3.fetchall()
+        print(test)
+        if not test:
             conn3.commit()
             bot.send_message(human_id, 'Я удалиль, теперь этого клоуна нет в твоих данных')
-
+        else:
+            bot.send_message(human_id, 'Ну я же попросил...Ошибка: такого пользователя нет')
     elif label == 3:
         tomorrow = (date.today() + timedelta(days=1)).strftime("%d.%m")
         if curs3.execute(
@@ -79,7 +78,7 @@ def message_reply(message):
     conn = sqlite3.connect('telegram_bot.db')
     curs = conn.cursor()
     human_id = message.chat.id
-    if human_id in mp.keys():
+    if human_id in mp:
         answer(mp[human_id], message, human_id)
     if message.text == "Добавить День Рождения":
         bot.send_message(human_id,
@@ -95,42 +94,29 @@ def message_reply(message):
     elif message.text == "Проверить Дни Рождения":
         mp[human_id] = 3
     elif message.text == "Вывести созданные данные":
-        curs.execute(
-            f'select note, d_birthday from birthdays where id_tel = {human_id}')
-        birthdays = curs.fetchall()
-        if birthdays:
-            for lines in birthdays:
+        if curs.execute(
+                'select note, d_birthday from birthdays where id_telegram = {hunam_id}'):
+            for lines in curs.fetchall():
                 bot.send_message(human_id, ''.join(FORM.format(line) for line in lines))
-        else:
-            bot.send_message(human_id, 'Никого неть :c')
     curs.close()
 
 
-def start1():
+def start():
     """Start out bot"""
     bot.polling(none_stop=True, interval=1)
 
 
-def birthday_check():
-    while True:
-        check()
-        sleep(5)
-
-
+@bot.message_handler(content_types='text')
 def check():
     """Check birthday"""
-    while True:
-        conn2 = sqlite3.connect('telegram_bot.db')
-        curs2 = conn2.cursor()
-        tomorrow = (date.today() + timedelta(days=1)).strftime("%d.%m")
-        curs2.execute(
-            f'select id_tel, note from birthdays where d_birthday = {tomorrow}')
-        for line in curs2.fetchall():
-            bot.send_message(line[0], f'Не забудь поздравить {line[1]} с Днём Рождеия')
-        bot.polling(none_stop=True, interval=2)
+    conn2 = sqlite3.connect('telegram_bot.db')
+    curs2 = conn2.cursor()
+    tomorrow = (date.today() + timedelta(days=1)).strftime("%d.%m.%Y")
+    curs2.execute(
+        f'select id_tel, note from birthdays where v_birth_day = {tomorrow}')
+    for line in curs2.fetchall():
+        bot.send_message(line[0], 'Не забудь поздравить ' + line[1] + ' с Днём Рождеия')
+    bot.polling(none_stop=True, interval=8)
 
 
-th = Thread(target=birthday_check())
-th.start()
-
-start1()
+start()
